0dbc288b536f84ee850c3272c71bb01b81444c45
Niraj Patel <nirajp82@yahoo.com>
2021-05-11 19:03:16 -0400

    Add project files.

---
+++ Controllers/CatalogController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Polly.Retry;
+using Polly;
+
+namespace PollyBefore.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class CatalogController : Controller
+    {
+        #region Members
+        readonly AsyncRetryPolicy<HttpResponseMessage> _httpAsyncRetryPolicy;
+        #endregion
+
+
+        #region Constructor
+        public CatalogController()
+        {
+            ////Basic Retry Policy
+            _httpAsyncRetryPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
+                            .RetryAsync(3);
+
+            ////Wait and Retry Policy
+        }
+        #endregion
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var httpClient = GetHttpClient();
+            string requestEndpoint = $"inventory/{id}";
+
+            HttpResponseMessage response = await _httpAsyncRetryPolicy.ExecuteAsync(() => httpClient.GetAsync(requestEndpoint));
+
+            if (response.IsSuccessStatusCode)
+            {
+                var itemsInStock = JsonSerializer.Deserialize<int>(await response.Content.ReadAsStringAsync());
+                return Ok(itemsInStock);
+            }
+
+            return StatusCode((int)response.StatusCode, response.Content.ReadAsStringAsync());
+        }
+
+        private HttpClient GetHttpClient()
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(@"http://localhost:57664/api/");
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient;
+        }
+    }
+}
---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System;
+
+namespace PollyBefore.Controllers
+{
+    [Route("api/Inventory")]
+    public class InventoryController : Controller
+    {
+        static int _requestCount = 0;
+        static string _message = "";
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            await Task.Delay(20);// simulate some data processing by delaying
+            _requestCount++;
+
+            if (_requestCount % 3 == 0) // only one of out 3 requests will succeed
+            {
+                _message = string.Empty;
+                _requestCount = 0;
+                return Ok(15);
+            }
+            _message += $" Attempt: {_requestCount} \r\n";
+            return StatusCode((int)HttpStatusCode.InternalServerError, $"{_message}");
+        }
+    }
+}
