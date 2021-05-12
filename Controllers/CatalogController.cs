f3b10d16fd528e397b726c58662b33efb3fb5a53
Niraj Patel <nirajp82@yahoo.com>
2021-05-11 20:33:31 -0400

    Added code to use Retry Delagate.

--- Controllers/CatalogController.cs
+++ Controllers/CatalogController.cs
@@ -9,6 +9,8 @@ using Polly.Retry;
 using Polly;
 using System.Linq;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Collections.Generic;
 
 namespace PollyBefore.Controllers
 {
@@ -18,6 +20,7 @@ namespace PollyBefore.Controllers
     {
         #region Members
         readonly AsyncRetryPolicy<HttpResponseMessage> _httpAsyncRetryPolicy;
+        readonly ILogger<CatalogController> _logger;
         #endregion
 
 
@@ -28,22 +31,16 @@ namespace PollyBefore.Controllers
             //_httpAsyncRetryPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
             //                .RetryAsync(3);
 
+
             ////Wait and Retry Policy
             //_httpAsyncRetryPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
             //                  .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromMilliseconds(100 * retryAttempt));
 
+
             //Retry Policy with onRetryAsync Delegate
             _httpAsyncRetryPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
-                            .RetryAsync(3, onRetryAsync: (httpResponseMessage, retryCount) =>
-                            {
-                                if (httpResponseMessage.Result.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                                    logger.LogError("Unauthorized");
-                                else if (httpResponseMessage.Result.StatusCode == System.Net.HttpStatusCode.NotFound)
-                                    logger.LogError("NotFound");
-                                else if (httpResponseMessage.Result.StatusCode == System.Net.HttpStatusCode.InternalServerError)
-                                    logger.LogError("InternalServerError");
-                                return Task.CompletedTask;
-                            });
+                            .RetryAsync(3, onRetryAsync: OnRetry);
+            this._logger = logger;
         }
         #endregion
 
@@ -51,28 +48,63 @@ namespace PollyBefore.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
-            var httpClient = GetHttpClient();
             string requestEndpoint = $"inventory/{id}";
 
-            HttpResponseMessage response = await _httpAsyncRetryPolicy.ExecuteAsync(() => httpClient.GetAsync(requestEndpoint));
+            var response = await _httpAsyncRetryPolicy.ExecuteAsync(context =>
+            {
+                string tokenValue = context["TokenValue"].ToString();
+                _logger.LogInformation($"Calling API: {tokenValue}");
+                var httpClient = GetHttpClient(tokenValue);
+                return httpClient.GetAsync(requestEndpoint);
+            }, new Dictionary<string, object>
+            {
+                {"TokenValue", "BadAuthToken"}
+            });
 
             if (response.IsSuccessStatusCode)
             {
                 var itemsInStock = JsonSerializer.Deserialize<int>(await response.Content.ReadAsStringAsync());
-                Response.Headers.Add("X-Debug-Message", response.Headers.GetValues("X-Debug-Message").FirstOrDefault());
                 return Ok(itemsInStock);
             }
 
             return StatusCode((int)response.StatusCode, response.Content.ReadAsStringAsync());
         }
 
-        private HttpClient GetHttpClient()
+        private Task OnRetry(DelegateResult<HttpResponseMessage> delegateResponse, int retryCnt, Context context)
         {
-            var httpClient = new HttpClient();
+            if (delegateResponse.Result.StatusCode == System.Net.HttpStatusCode.NotFound)
+                _logger.LogError("NotFound");
+            else if (delegateResponse.Result.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+                _logger.LogError("InternalServerError");
+            else if (delegateResponse.Result.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                context.Remove("TokenValue");
+                context.Add("TokenValue", "GoodAuthToken");
+                _logger.LogError("Unauthorized");
+            }
+            return Task.CompletedTask;
+        }
+
+        private HttpClient GetHttpClient(string authCookieValue)
+        {
+            var cookieContainer = new CookieContainer();
+            var handler = new HttpClientHandler() { CookieContainer = cookieContainer };
+            cookieContainer.Add(new Uri("http://localhost"), new Cookie("Auth", authCookieValue));
+
+            var httpClient = new HttpClient(handler);
             httpClient.BaseAddress = new Uri(@"http://localhost:57664/api/");
             httpClient.DefaultRequestHeaders.Accept.Clear();
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             return httpClient;
         }
+
+        //private HttpClient GetHttpClient()
+        //{
+        //    var httpClient = new HttpClient();
+        //    httpClient.BaseAddress = new Uri(@"http://localhost:57664/api/");
+        //    httpClient.DefaultRequestHeaders.Accept.Clear();
+        //    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        //    return httpClient;
+        //}
     }
 }
--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -2,30 +2,49 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System;
+using Microsoft.Extensions.Logging;
 
 namespace PollyBefore.Controllers
 {
     [Route("api/Inventory")]
     public class InventoryController : Controller
     {
+        #region Members
+        readonly ILogger<CatalogController> _logger;
         static int _requestCount = 0;
-        static string _message = "";
+        #endregion
 
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
+        {
+            return await AuthExample();
+            //return await RetryExample();
+        }
+
+        #region Private Methods
+        private async Task<IActionResult> RetryExample()
         {
             await Task.Delay(20);// simulate some data processing by delaying
             _requestCount++;
 
             if (_requestCount % 3 == 0) // only one of out 3 requests will succeed
             {
-                HttpContext.Response.Headers.Add("X-Debug-Message", _message);
-                _message = string.Empty;
                 _requestCount = 0;
                 return Ok(15);
             }
-            _message += $" Attempt {_requestCount}";
-            return StatusCode((int)HttpStatusCode.InternalServerError, $"{_message}");
+            return StatusCode((int)HttpStatusCode.InternalServerError, $"Please try again. Attempt {_requestCount}");
         }
+
+        private async Task<IActionResult> AuthExample()
+        {
+            await Task.Delay(20);// simulate some data processing by delaying
+
+            string authCode = Request.Cookies["Auth"];
+            if (authCode == "GoodAuthToken")
+                return Ok(15);
+            else
+                return StatusCode((int)HttpStatusCode.Unauthorized, $"Not authorized ");
+        }
+        #endregion
     }
 }
